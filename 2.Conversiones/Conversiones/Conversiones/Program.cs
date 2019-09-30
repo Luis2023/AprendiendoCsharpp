37afeb19a53c7b8295b640f25af38f2fe5145c39
Jose Luis Gonzalez Rodriguez <Luis Arturo Gonzalez Morales>
2019-09-30 18:01:14 -0500

    segundo programa (Conversiones)

---
+++ 2.Conversiones/Conversiones/Conversiones/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Conversiones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Numero = "1234";
+            Console.WriteLine(Numero.GetType().ToString());
+            int intNumero = Convert.ToInt32(Numero);
+            Console.WriteLine(intNumero.GetType().ToString());
+            Console.WriteLine(String.Format("El Numero es {0}", intNumero));
+            Console.ReadLine();
+
+
+        }
+    }
+}
