387c69844ec125b5db28095677ce1c01a1f6b809
Jose Luis Gonzalez Rodriguez <Luis Arturo Gonzalez Morales>
2019-09-30 18:46:37 -0500

    Cuarto Programa (detectar si un dato es entero)

---
+++ 4.DetectarElTipoDeDato/Detectar si un dato es entero/Detectar si un dato es entero/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Detectar_si_un_dato_es_entero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Valor;
+            int receptora = 0;
+
+            Console.WriteLine("Escribe algo Campeon ");
+            Valor = Console.ReadLine();
+
+            if (int.TryParse(Valor , out receptora))
+            {
+                //si se convierte se almacenara el dato 
+                Console.WriteLine(
+                    String.Format ("Dato Entero {0}. Muy bien tkm", receptora));
+            }
+            else
+            {
+                //si no se rechaza la entrada de datos
+                Console.WriteLine("Tienes que ingresar un dato entero e.e");
+            }
+
+            //Pausa
+            Console.WriteLine();
+            Console.WriteLine("Preciona INTRO Pa' continuar");
+            Console.ReadKey();
+        }
+    }
+}
