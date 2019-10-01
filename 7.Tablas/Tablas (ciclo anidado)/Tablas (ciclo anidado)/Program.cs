34a1c37c4a399e3a5ca780974e0e6c44a3fd7aa7
Jose Luis Gonzalez Rodriguez <Luis Arturo Gonzalez Morales>
2019-09-30 20:54:54 -0500

    Programa Siete (Ciclo anillado)

---
+++ 7.Tablas/Tablas (ciclo anidado)/Tablas (ciclo anidado)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tablas__ciclo_anidado_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("");
+                Console.WriteLine(String.Format("Tabla Del {0}", i));
+                Console.WriteLine("");
+                //un for dentro de otro for permite cambiar las variables de secuencia
+                for(int l=1; l<=10; l++)
+                {
+                    Console.WriteLine(
+                        string.Format("{0} * {1} = {2}",
+                        i, l, i * l));
+
+                }
+
+            }
+
+
+        }
+    }
+}
